9ba22c0efdec8fec20779fdf6abf7a87a478d769
glzcc <gl-zcc@qq.com>
2021-08-16 17:44:23 +0800

    Initial commit

---
+++ app/renderer/app.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import Title from './title';
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/">
+          <div>可视化简历平台</div>
+          <div>这是 Electron + React </div>
+          <Title styles={{
+            color: 'yellow'
+          }} text="123"/>
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
